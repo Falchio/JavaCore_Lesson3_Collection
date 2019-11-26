797c9404725439de5c8759d21246e1a7fd7fd1a7
Falchio <soulsard@gmail.com>
2019-11-26 21:34:21 +0300

    Fix array class

--- src/array/Main.java
+++ src/array/Main.java
@@ -22,7 +22,6 @@ public class Main {
             hmSomeWords.put(thisWord, hmSomeWords.getOrDefault(thisWord, 0) + 1);
         }
 
-        System.out.println(Arrays.toString(someWords));
         System.out.println(hmSomeWords);
     }
 
